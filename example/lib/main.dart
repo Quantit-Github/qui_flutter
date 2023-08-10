aafe25a3b0e61a9b0120099a444c80a722c2a2de
Quantit_LEE DO WON <dwlee@quantit.io>
2023-08-10 15:49:08 +0900

    PQ-11 example code 작성

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,37 +1,52 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:qui_flutter/core/material/qui_theme.dart';
+import 'package:qui_flutter/core/mode.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await QuiMaterialTheme.getThemeMode();
+
+  runApp(MyApp(themeMode: savedThemeMode));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    required this.themeMode,
+    super.key,
+  });
 
-  // This widget is the root of your application.
+  final QuiThemeMode? themeMode;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return QuiMaterialTheme(
+      // User custom theme (light, dark)
+      light: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.amber,
+      ),
+      initial: themeMode ?? QuiThemeMode.light,
+      builder: (theme, light, dark) {
+        log((light == dark).toString(), name: "light == dark");
+        log(dark.brightness.toString(), name: "dark");
+        log(light.brightness.toString(), name: "light");
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: theme,
+          home: FutureBuilder(
+              future: QuiMaterialTheme.getThemeMode(),
+              builder: (context, snapshot) {
+                log(snapshot.data?.modeName ?? "", name: "Current Theme Mode");
+                return MyHomePage(title: theme.brightness.toString());
+              }),
+        );
+      },
     );
   }
 }
@@ -82,9 +97,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Colors.amber),
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -116,7 +135,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          log("convert theme");
+          QuiMaterialTheme.of(context).toggleThemeMode();
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
